f32e4beff17205bfbc7f4598e1869198167bced1
Cannon-Cole <colecannonschool@gmail.com>
2022-06-03 22:10:35 -0600

    Added saved state to dice

--- src/App.js
+++ src/App.js
@@ -3,26 +3,28 @@ import Die from "./components/Die";
 import React from "react";
 
 function App() {
-  const [random_numbers, set_random_numbers] = React.useState(
-    random_ten_numbers()
-  );
-
-  function random_ten_numbers() {
-    let random_array = [];
-    for (let i = 0; i < 10; i++) {
-      random_array.push(Math.floor(Math.random() * 6) + 1);
-    }
-    return random_array;
-  }
+  const [random_numbers, set_random_numbers] = React.useState(new_dice());
 
   function roll_dice() {
-    set_random_numbers(random_ten_numbers());
+    set_random_numbers(new_dice());
   }
+
+  function new_dice() {
+    const newDice = [];
+    for (let i = 0; i < 10; i++) {
+      newDice.push({
+        value: Math.ceil(Math.random() * 6),
+        isHeld: false,
+      });
+    }
+    return newDice;
+  }
+
   return (
     <main>
       <div className="die-grid">
         {random_numbers.map((x) => (
-          <Die value={x} />
+          <Die value={x.value} />
         ))}
       </div>
       <button className="roll-button" type="button" onClick={roll_dice}>
