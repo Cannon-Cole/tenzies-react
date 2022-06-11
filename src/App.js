ba062c7ea8492b50aaa7ae51911c22a0b7e8e2d7
Cannon-Cole <colecannonschool@gmail.com>
2022-06-11 00:01:53 -0600

    Added ability to hold dice

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import "./App.css";
 import Die from "./components/Die";
 import React from "react";
+import { nanoid } from "nanoid";
 
 function App() {
   const [random_numbers, set_random_numbers] = React.useState(new_dice());
 
   function roll_dice() {
-    set_random_numbers(new_dice());
+    set_random_numbers((ran_nums) => {
+      return ran_nums.map((item) => {
+        if (!item.isHeld) {
+          return { ...item, value: Math.ceil(Math.random() * 6) };
+        } else {
+          return item;
+        }
+      });
+    });
   }
 
   function new_dice() {
@@ -15,16 +24,40 @@ function App() {
       newDice.push({
         value: Math.ceil(Math.random() * 6),
         isHeld: false,
+        id: nanoid(),
       });
     }
     return newDice;
   }
 
+  function die_click(die) {
+    set_random_numbers((ran_nums) => {
+      return ran_nums.map((item) => {
+        if (die === item.id) {
+          return { ...item, isHeld: !item.isHeld };
+        } else {
+          return item;
+        }
+      });
+    });
+  }
+
   return (
     <main>
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">
+        Roll until all dice are the same. Click each die to freeze it at its
+        current value between rolls.
+      </p>
       <div className="die-grid">
         {random_numbers.map((x) => (
-          <Die value={x.value} />
+          <Die
+            key={x.id}
+            value={x.value}
+            isHeld={x.isHeld}
+            die_click={die_click}
+            id={x.id}
+          />
         ))}
       </div>
       <button className="roll-button" type="button" onClick={roll_dice}>
--- src/components/Die.js
+++ src/components/Die.js
@@ -1,5 +1,16 @@
 function Die(props) {
-  return <div className="die">{props.value}</div>;
+  const styles = {
+    backgroundColor: props.isHeld ? "#59E391" : "white",
+  };
+  return (
+    <div
+      className={"die"}
+      style={styles}
+      onClick={() => props.die_click(props.id)}
+    >
+      {props.value}
+    </div>
+  );
 }
 
 export default Die;
