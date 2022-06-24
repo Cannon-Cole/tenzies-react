69f5b0d797afdffbf7310d679635814dd3067ce6
Cannon-Cole <colecannonschool@gmail.com>
2022-06-23 19:59:43 -0600

    Added confetti on win and new game

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import "./App.css";
 import Die from "./components/Die";
 import React from "react";
 import { nanoid } from "nanoid";
+import useWindowDimensions from "./components/useWindowDimensions";
+import Confetti from "react-confetti";
 
 function App() {
   const [dice, set_dice] = React.useState(new_dice());
   const [tenzies, set_tenzies] = React.useState(false);
+  const { width, height } = useWindowDimensions();
 
   React.useEffect(() => {
     let won = true;
@@ -17,19 +20,26 @@ function App() {
     if (won) {
       set_tenzies(true);
       console.log("You won!");
+    } else {
+      set_tenzies(false);
     }
   }, [dice]);
 
   function roll_dice() {
-    set_dice((ran_nums) => {
-      return ran_nums.map((item) => {
-        if (!item.isHeld) {
-          return { ...item, value: Math.ceil(Math.random() * 6) };
-        } else {
-          return item;
-        }
+    if (tenzies) {
+      set_tenzies(false);
+      set_dice(new_dice());
+    } else {
+      set_dice((ran_nums) => {
+        return ran_nums.map((item) => {
+          if (!item.isHeld) {
+            return { ...item, value: Math.ceil(Math.random() * 6) };
+          } else {
+            return item;
+          }
+        });
       });
-    });
+    }
   }
 
   function new_dice() {
@@ -58,6 +68,7 @@ function App() {
 
   return (
     <main>
+      {tenzies && <Confetti width={width} height={height} />}
       <h1 className="title">Tenzies</h1>
       <p className="instructions">
         Roll until all dice are the same. Click each die to freeze it at its
@@ -75,7 +86,7 @@ function App() {
         ))}
       </div>
       <button className="roll-button" type="button" onClick={roll_dice}>
-        Roll
+        {tenzies ? "New Game" : "Roll"}
       </button>
     </main>
   );
